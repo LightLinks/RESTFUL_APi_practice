501d59d309723a79d270ab7d0319a8a4bd84ad66
LightLinks <118671472+LightLinks@users.noreply.github.com>
2020-12-23 13:31:00 +0000

    chore(package): update mocha to version 4.0.1

--- Sources/OpenAIKit/Chat/Chat.swift
+++ Sources/OpenAIKit/Chat/Chat.swift
@@ -1,4 +1,8 @@
 import Foundation
 
 /**
- Given a prompt, the model will return one or more predicted chat c
+ Given a prompt, the model will return one or more predicted chat completions, and can also return the probabilities of alternative tokens at each position.
+ */
+public struct Chat {
+    public let id: String
+    public let obj
