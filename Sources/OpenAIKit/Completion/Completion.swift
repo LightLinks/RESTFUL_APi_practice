cd7886597fd26b79d4966ce0c41dffc12fb420b1
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-01-17 06:49:07 +0000

    LZ4 : fix the data abort issue

--- Sources/OpenAIKit/Completion/Completion.swift
+++ Sources/OpenAIKit/Completion/Completion.swift
@@ -1,4 +1,9 @@
 import Foundation
 
 /**
- Given a prompt, the model will return one or more predicted completions, and can also return the probabilities of 
+ Given a prompt, the model will return one or more predicted completions, and can also return the probabilities of alternative tokens at each position.
+ */
+public struct Completion {
+    public let id: String
+    public let object: String
+    public 
