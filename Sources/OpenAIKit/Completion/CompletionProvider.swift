8682ed80b2f4ed3fdbdfbaba04bdafd255572256
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-02-24 15:25:20 +0000

    SAMRAI:  working ...

--- Sources/OpenAIKit/Completion/CompletionProvider.swift
+++ Sources/OpenAIKit/Completion/CompletionProvider.swift
@@ -12,4 +12,11 @@ public struct CompletionProvider {
       
      https://api.openai.com/v1/completions
 
-     Creates a completion for the provided prompt and para
+     Creates a completion for the provided prompt and parameters
+     */
+    public func create(
+        model: ModelID,
+        prompts: [String] = [],
+        suffix: String? = nil,
+        maxTokens: Int = 16,
+        t
