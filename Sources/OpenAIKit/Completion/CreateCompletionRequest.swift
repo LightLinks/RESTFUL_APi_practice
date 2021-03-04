192bdb8993f121053318a57a191359eebe173064
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-03-04 07:42:05 +0000

    updated menu

--- Sources/OpenAIKit/Completion/CreateCompletionRequest.swift
+++ Sources/OpenAIKit/Completion/CreateCompletionRequest.swift
@@ -1,1 +1,8 @@
-import AsyncHTTPCl
+import AsyncHTTPClient
+import NIOHTTP1
+import Foundation
+
+struct CreateCompletionRequest: Request {
+    let method: HTTPMethod = .POST
+    let path = "/v1/completions"
+   
