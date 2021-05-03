6eb71fcbd767026bebf20bb8cdfba40be4f3ca7e
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-05-03 15:57:26 +0000

    added gui

--- Sources/OpenAIKit/Edit/EditProvider.swift
+++ Sources/OpenAIKit/Edit/EditProvider.swift
@@ -1,3 +1,13 @@
 public struct EditProvider {
     
-    private let requestHandler: RequestHandle
+    private let requestHandler: RequestHandler
+    
+    init(requestHandler: RequestHandler) {
+        self.requestHandler = requestHandler
+    }
+    
+    /**
+     Create edit
+     POST
+      
+     https:
