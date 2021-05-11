974ed0777d731b668e1f250ba20dba9a0cf09ba2
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-05-11 22:38:45 +0000

    check error

--- Sources/OpenAIKit/Edit/EditProvider.swift
+++ Sources/OpenAIKit/Edit/EditProvider.swift
@@ -14,4 +14,9 @@ public struct EditProvider {
 
      Creates a new edit for the provided input, instruction, and parameters
      */
-    public func cr
+    public func create(
+        model: ModelID = Model.GPT3.textDavinciEdit001,
+        input: String = "",
+        instruction: String,
+        n: Int = 1,
+        temper
