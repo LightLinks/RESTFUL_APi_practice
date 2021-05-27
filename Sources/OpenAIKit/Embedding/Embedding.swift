71a7a6e42c083d4ebd000f4c310e66a3d687f0f1
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-05-27 08:21:05 +0000

    kthread: Implement park/unpark facility

--- Sources/OpenAIKit/Embedding/Embedding.swift
+++ Sources/OpenAIKit/Embedding/Embedding.swift
@@ -1,4 +1,9 @@
 import Foundation
 
 /**
- Get a vector representation of a given input that can be easily co
+ Get a vector representation of a given input that can be easily consumed by machine learning models and algorithms.
+ */
+public struct Embedding {
+    public let object: String
+    public let embedding: [Float]
+    public let 
