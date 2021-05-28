f2356dece941f2c2d9e8f8f7566f9e58ced1a54a
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-05-28 14:02:39 +0000

    rebase fix

--- Sources/OpenAIKit/Embedding/EmbeddingProvider.swift
+++ Sources/OpenAIKit/Embedding/EmbeddingProvider.swift
@@ -2,3 +2,11 @@ public struct EmbeddingProvider {
     
     private let requestHandler: RequestHandler
     
+    init(requestHandler: RequestHandler) {
+        self.requestHandler = requestHandler
+    }
+    
+    /**
+     Create embeddings
+     POST
+ 
