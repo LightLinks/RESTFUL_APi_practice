c120bc43befd8a62f57b8942948e133451fb76da
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-06-02 13:51:50 +0000

    Update netlify.toml

--- Sources/OpenAIKit/Errors/APIError.swift
+++ Sources/OpenAIKit/Errors/APIError.swift
@@ -2,4 +2,10 @@ import Foundation
 
 public struct APIError: Error, Decodable {
     public let message: String
-  
+    public let type: String
+    public let param: String?
+    public let code: String?
+}
+
+public struct APIErrorResponse: Error, Decodable {
+    
