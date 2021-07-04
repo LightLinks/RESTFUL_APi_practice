45cfb5c1e7f9bc425cdb3c941f792fdbb0babaee
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-07-04 14:38:17 +0000

    gstreamer: update to git HEAD

--- Sources/OpenAIKit/File/Request/DeleteFileRequest.swift
+++ Sources/OpenAIKit/File/Request/DeleteFileRequest.swift
@@ -5,4 +5,8 @@ struct DeleteFileRequest: Request {
     let method: HTTPMethod = .DELETE
     let path: String
     
-    
+    init(id: String) {
+        self.path = "/v1/files/\(id)"
+    }
+}
+
---
+++ Sources/OpenAIKit/File/Request/ListFilesRequest.swift
@@ -0,0 +1,5 @@
+import NIOHTTP1
+import Foundation
+
+struct ListFilesRequest: Request {
+    
