c55375b8c35cf87172edc91881fb9f660bbf5730
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-07-15 06:55:25 +0000

    Added details

--- Sources/OpenAIKit/Image/ImageResponse.swift
+++ Sources/OpenAIKit/Image/ImageResponse.swift
@@ -2,4 +2,7 @@ import Foundation
 
 public struct ImageResponse {
     public let created: Date
-    public let data: [Im
+    public let data: [Image]
+}
+
+extension ImageResponse: Decodable {}
---
+++ Sources/OpenAIKit/Image/Requests/CreateImageRequest.swift
@@ -0,0 +1,3 @@
+import AsyncHTTPClient
+import NIOHTTP1
+imp
