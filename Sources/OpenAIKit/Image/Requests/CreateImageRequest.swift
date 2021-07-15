595746bc4c3ef9fd05296ffa14bd90056b932e05
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-07-15 21:40:28 +0000

    Update BinaryTree.hpp

--- Sources/OpenAIKit/Image/Requests/CreateImageRequest.swift
+++ Sources/OpenAIKit/Image/Requests/CreateImageRequest.swift
@@ -5,4 +5,13 @@ import Foundation
 struct CreateImageRequest: Request {
     let method: HTTPMethod = .POST
     let path = "/v1/images/generations"
-    let body: HTTPClie
+    let body: HTTPClient.Body?
+    
+    init(
+        prompt: String,
+        n: Int,
+        size: Image.Size,
+        user: String?
+    ) throws {
+        
+ 
