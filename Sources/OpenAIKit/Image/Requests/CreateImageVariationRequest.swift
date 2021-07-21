06cb44b87c2439f361dc3a500e88d5a24e910a1f
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-07-21 18:54:07 +0000

    Move to GitHub Actions (#2)

--- Sources/OpenAIKit/Image/Requests/CreateImageVariationRequest.swift
+++ Sources/OpenAIKit/Image/Requests/CreateImageVariationRequest.swift
@@ -6,4 +6,8 @@ struct CreateImageVariationRequest: Request {
     let method: HTTPMethod = .POST
     let path = "/v1/images/variations"
     let body: HTTPClient.Body?
-    private let boundary = UUID().uui
+    private let boundary = UUID().uuidString
+    
+    var headers: HTTPHeaders {
+        var headers = HTTPHeaders()
+        headers.add(name: "Content-Type", value: "multipart/form-data; boundary=\(bo
