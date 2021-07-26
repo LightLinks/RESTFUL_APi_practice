58613bda703fc7b830400a30f4061da40e0734b6
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-07-26 21:24:25 +0000

    publishing updates

--- Sources/OpenAIKit/Model/Provider/ModelProvider.swift
+++ Sources/OpenAIKit/Model/Provider/ModelProvider.swift
@@ -15,3 +15,26 @@ public struct ModelProvider {
 
      Lists the currently available models, and provides basic information about each one such as the owner and availability.
      */
+    public func list() async throws -> [Model] {
+        let request = ListModelsRequest()
+        
+        let response: ListModelsResponse = try await requestHandler.perform(request: request)
+        
+        return response.data
+    }
+    
+    /**
+     Retrieve model
+     GET
+      
+     https://api.openai.com/v1/models/{model}
+
+     Retrieves a model instance, providing basic information about the model such as the owner and permissioning.
+     */
+    public func retrieve(id: String) async throws -> Model {
+        let request = RetrieveModelRequest(id: id)
+    
+        return try await requestHandler.perform(request: request)
+    }
+    
+}
