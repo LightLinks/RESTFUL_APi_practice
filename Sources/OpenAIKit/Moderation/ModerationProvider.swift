dc1ed0d1925563369876aca07875616f36188272
LightLinks <118671472+LightLinks@users.noreply.github.com>
2021-08-17 22:00:38 +0000

    Build new website version

--- Sources/OpenAIKit/Moderation/ModerationProvider.swift
+++ Sources/OpenAIKit/Moderation/ModerationProvider.swift
@@ -2,4 +2,12 @@ public struct ModerationProvider {
     
     private let requestHandler: RequestHandler
     
-    init(requestHandler: RequestHan
+    init(requestHandler: RequestHandler) {
+        self.requestHandler = requestHandler
+    }
+    
+    /**
+     Create moderation
+     POST
+      
+     https://api.openai.co
